1cf1567d4effc2f2dcee8b3b9d0cd3d2cfccd13f
0Nikola0 <62570597+0Nikola0@users.noreply.github.com>
2020-10-01 21:24:38 +0200

    Add files via upload

---
+++ Disable-WIndows10-Updates.ps1
@@ -0,0 +1,15 @@
+# RUN AS ADMINISTRATOR
+## Stop Windows Update
+Stop-Service -Name wuauserv     
+
+## Disable auto startup for Windows Update
+Set-Service -Name wuauserv -StartupType Disabled
+## Use this instead of the first one if it doesn't work
+# Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Services\wuauserv' -Name 'Start' -value 4
+
+## Stop Windows Update Medic Service
+Stop-Service -Name WaaSMedicSvc
+
+## Optional disable auto startup for Windows Update Medic Service
+## Has to be done from Registry since Services doesn't allow it to be set to Disabled
+# Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc' -Name 'Start' -value 4
